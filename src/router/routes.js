f0b756b9e02e579423bfd5385407d06700af88aa
TumCucTom <TumCucTom@users.noreply.github.com>
2025-01-14 02:57:37 +0000

    Deploying to gh-pages from @ spe-uob/2024-MLAIPredictionMicroservices@317b445880c232ca45fbc3737d6848099b54dfc3 🚀

---
+++ src/router/routes.js
@@ -0,0 +1,26 @@
+import Home from "src/pages/Login.vue";
+import HomeLayout from "layouts/HomeLayout.vue";
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '',
+        component: () => import('pages/IndexPage.vue'),
+        component: HomeLayout,
+          children: [
+            { path: '', component: Home },
+          ]}
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
