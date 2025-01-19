2e184a8ff43ae5b653042201f9daad6cedd10d2a
Thomas Bale <tokbale@outlook.com>
2025-01-19 19:01:14 +0000

    docker updates

--- code/backend/python-server/server.py
+++ code/backend/python-server/server.py
@@ -18,7 +18,7 @@ db_config = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", ""),
-    "database": os.getenv("DB_NAME", "your_db_name")
+    "database": os.getenv("DB_NAME", "MLAIDB")
 }
 
 connection = pymysql.connect(**db_config)
