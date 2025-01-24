57b06b8dcb07a0c99c95cf99d94194dbfb1b5e4c
TumCucTom <152705691+TumCucTom@users.noreply.github.com>
2025-01-24 13:27:14 +0000

    Deploying to gh-pages from @ spe-uob/2024-MLAIPredictionMicroservices@999b841f92fc074766d497c1e16ec5ead4c9db92 🚀

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,6 +14,11 @@ const routes = [
           ]}
     ]
   },
+  { path: '/ResetPassword', component: () => import('src/pages/ResetPassword.vue') },
+  { path: '/VerifiedPassword', component: () => import('src/pages/VerifiedPassword.vue') },
+  { path: '/Dashboard', component: () => import('src/pages/DashBoard.vue') },
+  { path: '/OTPVerification', component: () => import('src/pages/OTPVerification.vue') },
+  { path: '/SetupResetPassword', component: () => import('src/pages/ForgotPassword.vue') },
 
   // Always leave this as last one,
   // but you can also remove it
