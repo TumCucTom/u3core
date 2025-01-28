0e79adaae177226e2deda496c4098beae636b17f
Thomas Bale <tokbale@outlook.com>
2025-01-28 17:57:26 +0000

    dump outside of docker too

--- code/backend/db/custom-shutdown.sh
+++ code/backend/db/custom-shutdown.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-# Define the output dump file path
+# Define the output dump file paths
 DUMP_FILE="/docker-entrypoint-initdb.d/dump.sql"
+LOCAL_DUMP_FILE="/initdb/dump.sql"
 
-echo "Exporting MySQL database to dump file..."
+echo "Exporting MySQL database to dump files..."
 
 # Perform the database dump
 mysqldump -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --all-databases > "${DUMP_FILE}"
 
-echo "Database successfully exported to ${DUMP_FILE}"
+# Also save a local copy of the dump
+cp "${DUMP_FILE}" "${LOCAL_DUMP_FILE}"
+
+echo "Database successfully exported to ${DUMP_FILE} and ${LOCAL_DUMP_FILE}"
