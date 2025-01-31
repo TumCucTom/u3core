b961eab899116eaeb9efc1642d5c5bea435ffa4b
Thomas Bale <tokbale@outlook.com>
2025-01-31 12:31:28 +0000

    added open cv import

--- code/backend/python-server/server.py
+++ code/backend/python-server/server.py
@@ -12,6 +12,7 @@ import json
 import time
 import logging
 import sys
+import cv2
 from flask import Flask, request, jsonify, send_from_directory
 import multiprocessing
 from fire_detection_script import process_rtsp_stream_with_url
