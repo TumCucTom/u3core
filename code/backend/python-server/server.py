a004a2a5233c388e6c8f11254d8d2b802200737d
Kanghyeon5468 <142624826+Kanghyeon5468@users.noreply.github.com>
2025-02-15 21:18:51 +0000

    Remove stream function

--- code/backend/python-server/server.py
+++ code/backend/python-server/server.py
@@ -261,48 +261,6 @@ def fetch_sites():
         return jsonify({"error": "Internal Server Error"}), 500
 
     
-@app.route('/api/stream',methods = ['POST'])
-def stream():
-    data = request.json
-    name = data.get('name')
-    rtsp_url = data.get('rtsp_url')
-
-    if not all([name, rtsp_url]):
-        return jsonify({"error": "Name and RTSP URL are required"}), 400
-
-    # Convert tcp:// to rtsp://
-    if rtsp_url.startswith("tcp://"):
-        rtsp_url = "rtsp://" + rtsp_url[6:]
-
-    video = cv2.VideoCapture(rtsp_url)
-    if not video.isOpened:
-        print("Error : Camera is not opened")
-    
-    last_alert = 0
-    alert_interval = 30
-
-    while True:
-        ret,frame = video.read()
-        if not ret:
-            break
-        fire_detected = detect_fire_with_roboflow(frame)
-        if fire_detected:
-            current_time = time.time()
-            if current_time - last_alert > alert_interval:
-                print("Sending message")
-                send_whatsapp_via_twilio(REC_WHATSAPP_NUMBER,ALERT_MESSAGE)
-                last_alert = current_time
-
-        cv2.imshow("webcam stream",frame)
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
-    video.release()
-    cv2.destroyAllWindows()
-if __name__ == "__main__":
-    stream()
-
-
-
 
 @app.route('/api/dbinfo', methods=['GET'])
 def get_db_info():
