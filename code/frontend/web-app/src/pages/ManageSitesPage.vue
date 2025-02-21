9d12d63203816ae7c099e97ea3a36d71f83c8723
Thomas Bale <tokbale@outlook.com>
2025-02-21 13:17:33 +0000

    updated api endpoints

--- code/frontend/web-app/src/pages/ManageSitesPage.vue
+++ code/frontend/web-app/src/pages/ManageSitesPage.vue
@@ -238,7 +238,7 @@ export default {
     },
     async saveNewRTSP() {
       try {
-        const response = await axios.post('http://127.0.0.1:3002/api/add-camera', {
+        const response = await axios.post('http://16.171.224.57:3002/api/add-camera', {
           name: this.newCamera.name,
           rtsp_url: this.newCamera.RTSPURL,
         });
@@ -255,7 +255,7 @@ export default {
     },
     async saveNewSite() {
       try {
-        const response = await axios.post('http://127.0.0.1:3002/add-site', {
+        const response = await axios.post('http://16.171.224.57:3002/add-site', {
           name: this.newSite.name,
           latitude: this.newSite.latitude,
           longitude: this.newSite.longitude,
@@ -268,7 +268,7 @@ export default {
     },
     async fetchSites() {
       try {
-        const response = await axios.get('http://127.0.0.1:3002/sites');
+        const response = await axios.get('http://16.171.224.57:3002/sites');
         this.sites = response.data.sites;
       } catch (error) {
         console.error('Error fetching sites:', error);
