b8d8e7c61776811d590fbb8d489eb53e112723d2
sean chen <vi23973@bristol.ac.uk>
2025-02-23 03:51:05 +0000

    add U3logo to the siderbar

--- code/frontend/web-app/src/layouts/SidebarLayout.vue
+++ code/frontend/web-app/src/layouts/SidebarLayout.vue
@@ -10,6 +10,7 @@
         <div class="col q-pt-md">
            <div class="q-pa-md q-mb-md" style="height: 60px;">
            <!-- space to put logo later -->
+            <q-img src="../assets/u3logo1.png" alt="Digital U3" style="width: 60px;" />
            </div>
        
         <!-- sidebar -->
@@ -133,7 +134,7 @@
               <div>user name</div>
               <div class="text-caption text-grey">username@email.com</div>
             </q-item-section>
-            
+
             <q-item-section side>
               <q-icon name="exit_to_app" />
             </q-item-section>
