23b912b73de8449c1560fc86f971714224831d4c
TumCucTom <152705691+TumCucTom@users.noreply.github.com>
2025-03-06 13:48:29 +0000

    Deploying to gh-pages from @ spe-uob/2024-MLAIPredictionMicroservices@53c0300e87503237ef9d1300b16ec57d9d3bbc4b 🚀

--- assets/MinimalLayout.b1bf0d55.js
+++
@@ -1,1 +0,0 @@
-import{Q as r,a as t}from"./QLayout.9a310c1f.js";import{_ as n,r as c,o as s,c as _,w as e,a}from"./index.5452fe19.js";const i={};function l(f,u){const o=c("router-view");return s(),_(r,{view:"lHh Lpr lFf"},{default:e(()=>[a(t,null,{default:e(()=>[a(o)]),_:1})]),_:1})}var d=n(i,[["render",l]]);export{d as default};
