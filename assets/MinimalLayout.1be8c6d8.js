01fd368b07624e189dfa8959ffb68bc9b4936f02
William23292 <158285343+William23292@users.noreply.github.com>
2025-03-28 16:41:12 +0000

    Deploying to gh-pages from @ spe-uob/2024-MLAIPredictionMicroservices@928ac40708e6a46b8fd941bbad867567335b3bc1 🚀

--- assets/MinimalLayout.1be8c6d8.js
+++
@@ -1,1 +0,0 @@
-import{Q as r,a as t}from"./QLayout.73cefa9f.js";import{_ as n,r as c,o as s,c as _,w as e,a}from"./index.1c752bc0.js";const i={};function l(f,u){const o=c("router-view");return s(),_(r,{view:"lHh Lpr lFf"},{default:e(()=>[a(t,null,{default:e(()=>[a(o)]),_:1})]),_:1})}var d=n(i,[["render",l]]);export{d as default};
