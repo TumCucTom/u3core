7832e3d6d298246e78b8594ebc41ae5b24357565
justice-123 <mintah166@gmail.com>
2025-03-31 13:33:41 +0100

    linting database.py

--- code/backend/python-server/database.py
+++ code/backend/python-server/database.py
@@ -66,7 +66,7 @@ def initialise_database(db_config):
 
         conn.commit()
         logging.info("Database tables initialized successfully")
-    except Exception as e:
-        logging.error(f"Error initializing database tables: {e}")
+    except Exception as error:
+        logging.error("Error initializing database tables: %(error)s", {"error": error})
     finally:
         conn.close()
