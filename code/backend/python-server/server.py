4f6138f61dab08a6fb85cc2f49d2a3642bde4f15
Thomas Bale <tokbale@outlook.com>
2025-04-25 23:54:43 +0100

    typo

--- code/backend/python-server/server.py
+++ code/backend/python-server/server.py
@@ -5,7 +5,6 @@
 # pylint: disable=c-extension-no-member
 # pylint: disable=too-many-locals
 # pylint: too-many-arguments
-# pylint: too-many-positional-arguments
 import os
 import random
 import string
